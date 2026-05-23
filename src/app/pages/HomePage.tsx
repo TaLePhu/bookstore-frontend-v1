a985e183e7467b52012ddbe64778f3f1abba45f2
HiepHinh <ngochiep240520@gmail.com>
2026-05-23 20:11:47 +0700

    fix: home

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -16,7 +16,7 @@ export function HomePage() {
       <BestSellers />
       <BookFinder />
       <NewBooks />
-      <AIRecommendation />
+      {/* <AIRecommendation /> */}
       {/* <Categories /> */}
     </>
   );
